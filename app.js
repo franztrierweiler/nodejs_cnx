94eb7c027ac2786dae1ee41c94f790c43a218454
franz <trierweilerfranz@gmail.com>
2019-02-24 17:02:03 +0000

    Ajout du support de CORS

--- app.js
+++ app.js
@@ -3,8 +3,27 @@ const http = require('http')
 const port = 3000
 
 const requestHandler = (request, response) => {
+  
+  console.log ('-- Request info: request.headers.origin');
+	console.log(request.headers.origin);
+	console.log ('-- Request info:');
+  
+  response.setHeader('Access-Control-Allow-Origin', '*');
+	response.setHeader('Access-Control-Request-Method', '*');
+	response.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
+	
+	// Ne retourner Access-Control-Allow-Headers que si headers contient origin
+	if (request.headers.origin != null)
+	  response.setHeader('Access-Control-Allow-Headers', request.headers.origin);
+	
+	if (request.method === 'OPTIONS') {
+		response.writeHead(200);
+		response.end();
+		return;
+	}
+  
   console.log(request.url)
-  response.end('Hello Node.js Server!')
+  response.end('Je suis Node.js Server!')
 }
 
 const server = http.createServer(requestHandler)
